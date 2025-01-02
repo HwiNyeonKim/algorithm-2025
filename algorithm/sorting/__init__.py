84b84692cbd826d8a99214cef6fa572e3a79b058
HwiNyeonKim <z2b23173us@gmail.com>
2025-01-02 10:56:40 +0900

    lint

--- algorithm/sorting/__init__.py
+++ algorithm/sorting/__init__.py
@@ -1,6 +1,4 @@
-from .가장_큰_수 import solution as solution_1
 from .h_index import solution as solution_2
+from .가장_큰_수 import solution as solution_1
 
-__all__ = [
-    "solution_1", "solution_2"
-]
+__all__ = ["solution_1", "solution_2"]
