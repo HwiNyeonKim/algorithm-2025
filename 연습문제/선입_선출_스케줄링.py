19a6408f000127aba8b9ca4862f9f502cc706204
HwiNyeonKim <z2b23173us@gmail.com>
2025-01-31 20:15:07 +0900

    전체 작업을 처리하는데 걸리는 시간을 먼저 계산하고, 이를 기반으로 풀이하도록 수정
    - 전체 작업을 처리하는데 걸리는 시간(t)를 알면, 다음을 알 수 있다.
        - t - 1까지 처리된 작업의 수
        - t에서 idle 상태인 코어의 인덱스
    - 이를 기반으로 풀이하도록 수정

--- 연습문제/선입_선출_스케줄링.py
+++ 연습문제/선입_선출_스케줄링.py
@@ -1,4 +1,21 @@
-import math
+def calc_tasks_on_turn(turn, cores):
+    return sum((turn // core) + 1 for core in cores)
+
+
+def calc_minimum_turn_to_finish_tasks(n, cores):
+    minimum_turn = 0
+    maximum_turn = 10_000 * 50_000  # 작업 처리 시간 최대값 * 일의 개수 최대값
+
+    while minimum_turn < maximum_turn:
+        mid = (minimum_turn + maximum_turn) // 2
+        handled_tasks = calc_tasks_on_turn(mid, cores)
+
+        if handled_tasks < n:
+            minimum_turn = mid + 1
+        else:
+            maximum_turn = mid
+
+    return min(minimum_turn, maximum_turn)
 
 
 def get_idle_cores(turn, cores):
@@ -11,18 +28,12 @@ def get_idle_cores(turn, cores):
 
 
 def solution(n, cores):
-    turn = 0
+    minimum_turn_to_finish = calc_minimum_turn_to_finish_tasks(n, cores)
 
-    cycle_turns = math.lcm(*cores)
-    tasks_per_cycle = sum(cycle_turns // core for core in cores)
-    while n > tasks_per_cycle:
-        n -= tasks_per_cycle
+    # 모든 작업을 처리하는데 필요한 시간 - 1일 때, 몇 개의 작업이 남았는지 확인한다.
+    n -= calc_tasks_on_turn(minimum_turn_to_finish - 1, cores)
 
-    while n > 0:
-        idle_cores = get_idle_cores(turn, cores)
+    # 마지막으로 할당될 작업들을 할당받을 수 있는 코어들을 확인한다.
+    idle_cores = get_idle_cores(minimum_turn_to_finish, cores)
 
-        n -= len(idle_cores)
-
-        turn += 1
-
-    return idle_cores[n + len(idle_cores) - 1]
+    return idle_cores[n - 1]
