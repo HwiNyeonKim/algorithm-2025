5aa16e495466779bf3aac845ba9e8a14e3f3bdf5
HwiNyeonKim <z2b23173us@gmail.com>
2025-01-31 23:39:48 +0900

    solve 하노이의 탑

--- 연습문제/하노이의_탑.py
+++ 연습문제/하노이의_탑.py
@@ -1,2 +1,14 @@
 def solution(n):
-    return list(list())
+    answer = list()
+
+    def hanoi(disk_to_move, from_, via, to):
+        if disk_to_move == 1:
+            answer.append([from_, to])
+        else:
+            hanoi(disk_to_move - 1, from_=from_, via=to, to=via)
+            answer.append([from_, to])
+            hanoi(disk_to_move - 1, from_=via, via=from_, to=to)
+
+    hanoi(n, 1, 2, 3)
+
+    return answer
