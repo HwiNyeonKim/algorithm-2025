fb34e395328d38e8d0874d55c63875fd7c34ddbd
HwiNyeonKim <z2b23173us@gmail.com>
2025-01-31 22:09:36 +0900

    Heap - 디스크 컨트롤러 문제풀이 #1

--- heap/test/test_디스크_컨트롤러.py
+++ heap/test/test_디스크_컨트롤러.py
@@ -1,6 +1,21 @@
+import pytest
+
 from heap import solution_disk_controller
 
 
-def test_disk_controller():
-    answer = solution_disk_controller([[0, 3], [1, 9], [2, 6]])
-    assert answer == 8
+@pytest.mark.parametrize(
+    "jobs, expected",
+    [
+        [[[0, 3], [1, 9], [2, 6]], 9],
+        [[[0, 9], [0, 4], [0, 5], [0, 7], [0, 3]], 13],
+        [[[1, 9], [1, 4], [1, 5], [1, 7], [1, 3]], 13],
+        [[[0, 5], [1, 2], [5, 5]], 6],
+        [[[0, 3], [1, 9], [500, 6]], 6],
+        [[[0, 4], [0, 3], [0, 2], [0, 1]], 5],
+        [[[0, 20], [3, 4], [3, 5], [17, 2]], 19],
+        [[[0, 5], [1, 5], [2, 4], [6, 1]], 7],
+    ],
+)
+def test_disk_controller(jobs, expected):
+    answer = solution_disk_controller(jobs)
+    assert answer == expected
--- heap/디스크_컨트롤러.py
+++ heap/디스크_컨트롤러.py
@@ -1,2 +1,46 @@
+import heapq
+
+
 def solution(jobs):
-    return 0
+    time_consumed = 0
+
+    shortest_job_priority_queue = list()
+    time_priority_queue = list()
+    for request_time, processing_time in jobs:
+        heapq.heappush(time_priority_queue, (request_time, processing_time))
+
+    start_time = 0
+    while time_priority_queue:
+        request_time, processing_time = heapq.heappop(time_priority_queue)
+
+        if start_time < request_time:
+            heapq.heappush(
+                time_priority_queue, (request_time, processing_time)
+            )
+
+            if not shortest_job_priority_queue:
+                start_time = request_time
+            else:
+                # run processing
+                shortest_processing_time, requested_time = heapq.heappop(
+                    shortest_job_priority_queue
+                )
+                end_time = start_time + shortest_processing_time
+                time_consumed += end_time - requested_time
+
+                start_time = end_time
+        else:
+            heapq.heappush(
+                shortest_job_priority_queue, (processing_time, request_time)
+            )
+
+    while shortest_job_priority_queue:
+        shortest_processing_time, requested_time = heapq.heappop(
+            shortest_job_priority_queue
+        )
+        end_time = start_time + shortest_processing_time
+        time_consumed += end_time - requested_time
+
+        start_time = end_time
+
+    return time_consumed // len(jobs)
