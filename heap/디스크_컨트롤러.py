356bb043eaf518f8519abbbf595f55025515b1ed
HwiNyeonKim <z2b23173us@gmail.com>
2025-01-31 23:05:45 +0900

    refactor 디스크 컨트롤러

--- heap/디스크_컨트롤러.py
+++ heap/디스크_컨트롤러.py
@@ -2,45 +2,33 @@ import heapq
 
 
 def solution(jobs):
+    job_queue = sorted(jobs)
+    priority_queue = list()
+    current_time = 0
     time_consumed = 0
 
-    shortest_job_priority_queue = list()
-    time_priority_queue = list()
-    for request_time, processing_time in jobs:
-        heapq.heappush(time_priority_queue, (request_time, processing_time))
+    def _handle_task():
+        nonlocal current_time, time_consumed
+        processing_time, request_time = heapq.heappop(priority_queue)
+        current_time += processing_time
+        time_consumed += current_time - request_time
 
-    start_time = 0
-    while time_priority_queue:
-        request_time, processing_time = heapq.heappop(time_priority_queue)
+    while job_queue:
+        # 1. 현재까지 도착한 작업들을 전부 priority queue에 넣는다.
+        while job_queue and job_queue[0][0] <= current_time:
+            request_time, processing_time = job_queue.pop(0)
+            heapq.heappush(priority_queue, (processing_time, request_time))
 
-        if start_time < request_time:
-            heapq.heappush(
-                time_priority_queue, (request_time, processing_time)
-            )
-
-            if not shortest_job_priority_queue:
-                start_time = request_time
-            else:
-                # run processing
-                shortest_processing_time, requested_time = heapq.heappop(
-                    shortest_job_priority_queue
-                )
-                end_time = start_time + shortest_processing_time
-                time_consumed += end_time - requested_time
-
-                start_time = end_time
+        # 2. priority queue에서 가장 짧은 작업을 꺼내서 처리한다.
+        if priority_queue:
+            _handle_task()
         else:
-            heapq.heappush(
-                shortest_job_priority_queue, (processing_time, request_time)
-            )
+            # 대기중인 작업이 없으면 다음번 작업까지 점프한다.
+            if job_queue:
+                current_time = job_queue[0][0]
 
-    while shortest_job_priority_queue:
-        shortest_processing_time, requested_time = heapq.heappop(
-            shortest_job_priority_queue
-        )
-        end_time = start_time + shortest_processing_time
-        time_consumed += end_time - requested_time
-
-        start_time = end_time
+    # 3. 남은 작업들을 전부 처리한다.
+    while priority_queue:
+        _handle_task()
 
     return time_consumed // len(jobs)
