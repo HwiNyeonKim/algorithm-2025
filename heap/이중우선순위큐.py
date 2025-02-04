d2372ed4d5325d999abfe624a3e144b40141b6a6
HwiNyeonKim <z2b23173us@gmail.com>
2025-02-04 21:07:16 +0900

    Heap - 이중우선순위큐 개선 2

--- heap/이중우선순위큐.py
+++ heap/이중우선순위큐.py
@@ -2,22 +2,25 @@ import heapq
 from collections import defaultdict
 
 
-def pop(heap, element_counter, sign):
-    while heap and element_counter[sign * heap[0]] == 0:
-        heapq.heappop(heap)
-
-    value = 0
-    if heap:
-        value = sign * heapq.heappop(heap)
-        element_counter[value] -= 1
-
-    return value
-
-
 def solution(operations):
     min_heap = list()
     max_heap = list()
     element_counter = defaultdict(int)
+    counter = 0
+
+    def pop(heap, sign):
+        nonlocal counter
+
+        while heap and element_counter[sign * heap[0]] == 0:
+            heapq.heappop(heap)
+
+        value = 0
+        if heap:
+            value = sign * heapq.heappop(heap)
+            element_counter[value] -= 1
+            counter -= 1
+
+        return value
 
     for operation in operations:
         if operation.startswith("I"):
@@ -26,21 +29,26 @@ def solution(operations):
             heapq.heappush(min_heap, value)
             heapq.heappush(max_heap, -value)
             element_counter[value] += 1
+            counter += 1
 
         elif operation == "D 1":
             # Pop maximum
-            pop(max_heap, element_counter, -1)
+            pop(max_heap, -1)
 
         elif operation == "D -1":
             # Pop minimum
-            pop(min_heap, element_counter, 1)
+            pop(min_heap, 1)
 
         else:
             # should not reach here
             raise Exception
 
-    maximum = pop(max_heap, element_counter, -1)
+        if counter == 0:
+            max_heap = list()
+            min_heap = list()
+
+    maximum = pop(max_heap, -1)
     element_counter[maximum] += 1
-    minimum = pop(min_heap, element_counter, 1)
+    minimum = pop(min_heap, 1)
 
     return [maximum, minimum]
