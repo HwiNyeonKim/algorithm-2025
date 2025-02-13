4dec9edcd9604d431d5073cd3bf910dcc6e0a281
HwiNyeonKim <z2b23173us@gmail.com>
2025-02-14 00:10:40 +0900

    홀짝 트리 Try #2: 성능 개선 1

--- programmers_code_challenge_2025/홀짝트리.py
+++ programmers_code_challenge_2025/홀짝트리.py
@@ -1,10 +1,5 @@
+from collections import defaultdict
 from enum import Enum
-
-
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.connected = set()
 
 
 class TreeType(Enum):
@@ -13,19 +8,16 @@ class TreeType(Enum):
     NONE = 2  # 트리 타입 없음
 
 
-tree_types = dict()  # key: (current node, parent node), value: TreeType
-
-
-def get_tree_type(node, parent):
-    children = node.connected - set([parent])
-
+def get_tree_type(node, parent, connections, tree_types):
     if (node, parent) not in tree_types:
         # 현재 노드가 (역)홀수/짝수 노드이고,
         # 이 노드의 각 children을 root로 하는 subtree가 모두 (역)홀짝 트리이면
         # tree_type[(node, parent)]는 (역)홀짝 트리이다.
-        if node.value % 2 == len(children) % 2:  # odd/even node
+        children = connections[node] - set([parent])
+        if node % 2 == len(children) % 2:  # odd/even node
             if all(
-                get_tree_type(child, node) == TreeType.ODD_EVEN
+                get_tree_type(child, node, connections, tree_types)
+                == TreeType.ODD_EVEN
                 for child in children
             ):
                 tree_types[(node, parent)] = TreeType.ODD_EVEN
@@ -33,7 +25,8 @@ def get_tree_type(node, parent):
                 tree_types[(node, parent)] = TreeType.NONE
         else:  # reversed odd/even node
             if all(
-                get_tree_type(child, node) == TreeType.REVERSED
+                get_tree_type(child, node, connections, tree_types)
+                == TreeType.REVERSED
                 for child in children
             ):
                 tree_types[(node, parent)] = TreeType.REVERSED
@@ -50,21 +43,18 @@ def solution(nodes, edges):
     - 하지만, 매번 전체 forest를 다시 탐색한다면 O(n^2)이 되어 시간초과이다.
     ! Cycle은 없다고 가정하고 풀이를 진행 해 본다.
     """
-    # Forest setup
-    forest = dict()
-    for node in nodes:
-        forest[node] = Node(node)
-
+    connections = defaultdict(set)
     for edge in edges:
         left, right = edge
-        forest[left].connected.add(forest[right])
-        forest[right].connected.add(forest[left])
+        connections[left].add(right)
+        connections[right].add(left)
 
     # Solve
-    answer = [0, 0, 0]
-    dummy = Node(0)
-    for n in forest.values():
-        tree_type = get_tree_type(n, dummy)
+    answer = [0, 0, 0]  # 홀짝트리, 역홀짝트리, 타입없음
+    dummy = 0
+    tree_types = dict()  # key: (current node, parent node), value: TreeType
+    for node in nodes:
+        tree_type = get_tree_type(node, dummy, connections, tree_types)
         answer[tree_type.value] += 1
 
     return answer[:2]
