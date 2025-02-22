c36d24c83f36c6697e8d6bebde6d63213e8a8121
HwiNyeonKim <z2b23173us@gmail.com>
2025-02-22 17:08:52 +0900

    봉인된 주문 헬퍼 함수 추가

--- programmers_code_challenge_2025/봉인된_주문.py
+++ programmers_code_challenge_2025/봉인된_주문.py
@@ -1,2 +1,36 @@
+def alphabet_to_int(char: str):
+    if len(char) != 1:
+        raise Exception
+
+    return ord(char) - ord("a") + 1
+
+
+def int_to_alphabet(index: int):
+    return chr(index + 96)
+
+
+def get_spell(index: int):
+    spell = list()
+
+    while index >= 0:
+        if index == 0:
+            break
+
+        char_int = (index - 1) % 26
+        spell.append(int_to_alphabet(char_int + 1))
+        index = (index - 1) // 26
+
+    spell.reverse()
+    return "".join(spell)
+
+
+def get_index(spell: str):
+    index = 0
+    for power, char in enumerate(spell[::-1]):
+        index += alphabet_to_int(char) * 26**power
+
+    return index
+
+
 def solution(n, bans):
     return ""
