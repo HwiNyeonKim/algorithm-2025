51d94b2527dcb2c5d7177b64d8e161bb29f9d64e
HwiNyeonKim <z2b23173us@gmail.com>
2025-02-22 17:48:56 +0900

    remove redundant code

--- programmers_code_challenge_2025/봉인된_주문.py
+++ programmers_code_challenge_2025/봉인된_주문.py
@@ -1,7 +1,4 @@
 def alphabet_to_int(char: str):
-    if len(char) != 1:
-        raise Exception
-
     return ord(char) - ord("a") + 1
 
 
@@ -12,10 +9,7 @@ def int_to_alphabet(index: int):
 def get_spell(index: int):
     spell = list()
 
-    while index >= 0:
-        if index == 0:
-            break
-
+    while index > 0:
         char_int = (index - 1) % 26
         spell.append(int_to_alphabet(char_int + 1))
         index = (index - 1) // 26
