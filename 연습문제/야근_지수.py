e313c6039e900cc233620b0be8047e46103c5e0a
HwiNyeonKim <z2b23173us@gmail.com>
2025-03-08 18:46:51 +0900

    야근 지수 SOLVED

--- 연습문제/야근_지수.py
+++ 연습문제/야근_지수.py
@@ -1,3 +1,15 @@
+from heapq import heappop, heappush
+
+
 def solution(n, works):
-    answer = 0
-    return answer
+    max_heap = list()
+    for work in works:
+        heappush(max_heap, -work)
+
+    for _ in range(n):
+        work = heappop(max_heap)
+        if work < 0:
+            work += 1
+        heappush(max_heap, work)
+
+    return sum(remaining_work**2 for remaining_work in max_heap)
