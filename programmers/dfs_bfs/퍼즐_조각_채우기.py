b285ff3cc20b12674fbadfd5b2a4a1a9a8de9a53
HwiNyeon Kim <z2b23173us@gmail.com>
2025-05-08 20:00:37 +0900

    [Programmers] 퍼즐 조각 채우기 - Solved

--- programmers/dfs_bfs/퍼즐_조각_채우기.py
+++ programmers/dfs_bfs/퍼즐_조각_채우기.py
@@ -1,7 +1,3 @@
-from typing import List
-from pprint import pprint
-
-
 DIRS = [
     [0, -1],
     [0, 1],
@@ -10,8 +6,12 @@ DIRS = [
 ]
 
 
-def find_areas(grid: List[List[int]], target: int):
+def find_areas(grid, target):
     """
+    Args:
+        grid (List[List[int]]): 2차원 배열
+        target (int): 찾고자 하는 값
+
     Returns:
         List[List[List[int]]]: 주어진 grid에서 target 값을 가진 블록들의 좌표들을 반환한다.
                         하나의 도형은 하나의 List[List[int]] 이다.
@@ -46,82 +46,64 @@ def find_areas(grid: List[List[int]], target: int):
     return areas
 
 
-def normalize(block: List[List[int]]):
+def shift_to_origin(block):
     """
+    Args:
+        block (List[List[int]]): 블록의 좌표 리스트
+
     Returns:
-        Tuple[Tuple[int, int]]:
-            가장 x와 y값이 작은 블록의 좌표를 (0, 0)으로 삼고
-            나머지 좌표들을 상대적으로 변환한 블록의 좌표를 반환한다.
-            단, 원점으로부터의 거리가 같은 1칸 블록이 2개 이상일 경우 x값이 작은 것을 기준으로 한다.
+        List[List[int, int]]: 원점에서 가장 가까운 좌표를 원점으로 이동시킨 블록의 좌표 리스트
+                            x 좌표를먼저 비교하고, 그 다음 y좌표를 비교한다.
     """
-    min_x = min(block, key=lambda x: x[0])[0]
-    min_y = min(block, key=lambda x: x[1])[1]
+    origin = min(block, key=lambda point: (point[0], point[1]))
+    shifted = [(x - origin[0], y - origin[1]) for x, y in block]
+    shifted.sort()
 
-    return tuple(sorted((x - min_x, y - min_y) for x, y in block))
+    return shifted
 
 
-def rotate_block(block: List[List[int]]):
+def get_rotated_blocks(block):
     """
+    Args:
+        block (List[List[int]]): 블록의 좌표 리스트
+
     Returns:
-        Tuple[Tuple[int, int]]: 90-degree rotated block (ccw)
+        List[List[List[int, int]]]: 90, 180, 270-deg 만큼 ccw 회전한 블록들의 좌표 리스트
     """
-    return tuple((-y, x) for x, y in block)
 
+    def _rotate(block):
+        return [(-y, x) for x, y in block]
 
-def is_fit(area: List[List[int]], candidates):
-    """
-    Returns:
-        bool: area의 모양이 candidates에 포함되는지를 찾는다.
-    """
-    area_set = set(tuple(point) for point in area)
+    rotated_blocks = [block]
+    for _ in range(3):
+        rotated_blocks.append(_rotate(rotated_blocks[-1]))
 
-    for candidate in candidates:
-        if len(area) != len(candidate):
-            continue
-
-        if area_set == set(candidate):
-            return True
-
-    return False
+    return rotated_blocks
 
 
 def solution(game_board, table):
-    # 1. board에서 빈 칸 찾기
-    # - 빈 칸 좌표들을 찾은 후, x와 y가 가장 작은 값을 기준으로 normalize 한다.((0,0)으로 설정)
-    # 2. table에서 도형 형태 찾기
-    # - 마찬가지로 각 도형들을 찾은 후 x와 y가 가장 작은 값을 기준으로 normalize 한다.
-    # - 이 도형들을 다시 90, 180, 270도 회전시킨 후 또 normalize 한다.
-    # 3. 1에서 찾은 결과가 2에서 만든 결과에 속해있으면 해당 블록을 놓을 수 있다(count 증가)
-    # 4. 최종 count를 반환한다.
     empty_areas = find_areas(game_board, 0)
+    shifted_empty_areas = [shift_to_origin(area) for area in empty_areas]
+
     blocks = find_areas(table, 1)
-
-    candidates = set()
+    all_block_shapes = list()
     for block in blocks:
-        quater_rotated_block = rotate_block(block)
-        half_rotated_block = rotate_block(quater_rotated_block)
-        three_quater_rotated_block = rotate_block(half_rotated_block)
-
-        candidates.update(
-            {
-                normalize(block),
-                normalize(quater_rotated_block),
-                normalize(half_rotated_block),
-                normalize(three_quater_rotated_block),
-            }
+        all_block_shapes.append(
+            [shift_to_origin(rotated) for rotated in get_rotated_blocks(block)]
         )
 
-    filled_areas = [False] * len(empty_areas)
+    shifted_empty_areas.sort(key=lambda x: len(x), reverse=True)
 
     count = 0
-    for i, area in enumerate(empty_areas):
-        if filled_areas[i]:
-            continue
+    block_used = [False] * len(all_block_shapes)
+    for area in shifted_empty_areas:
+        for i, block_shapes in enumerate(all_block_shapes):
+            if block_used[i]:
+                continue
 
-        normalized_area = normalize(area)
-        if is_fit(normalized_area, candidates):
-            filled_areas[i] = True
-            count += len(normalized_area)
-            break
+            if area in block_shapes:
+                block_used[i] = True
+                count += len(area)
+                break
 
     return count
