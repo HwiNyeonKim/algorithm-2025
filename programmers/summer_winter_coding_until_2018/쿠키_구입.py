d27fbdc8c0da800d6b7540380e7b529cff61e57b
HwiNyeon Kim <z2b23173us@gmail.com>
2025-05-14 19:47:09 +0900

    [Programmers] 쿠키 구입 - 로직개선 Solved

--- programmers/summer_winter_coding_until_2018/쿠키_구입.py
+++ programmers/summer_winter_coding_until_2018/쿠키_구입.py
@@ -1,24 +1,26 @@
 def solution(cookie):
-    # DP문제: i번부터 j번 까지의 쿠키의 합 계산
     n = len(cookie)
-    dp = [[0] * n for _ in range(n)]
-
-    for index, cookies in enumerate(cookie):
-        dp[index][index] = cookies
+    prefix_sum = [0] * (n + 1)
 
     for i in range(n):
-        for j in range(i + 1, n):
-            dp[i][j] += dp[i][j - 1] + cookie[j]
+        prefix_sum[i + 1] = prefix_sum[i] + cookie[i]  # prefix_sum[0] = 0
 
-    maximum_cookies = 0
-    for i in range(n - 1):
-        for j in range(i, n - 1):
-            left = dp[i][j]
+    max_cookie = 0
+    for m in range(n - 1):
+        l = m  # noqa
+        r = m + 1
 
-            for k in range(j + 1, n):
-                right = dp[j + 1][k]
+        while 0 <= l and r < n:
+            left = prefix_sum[m + 1] - prefix_sum[l]  # [l + 1, m]
+            right = prefix_sum[r + 1] - prefix_sum[m + 1]  # [m + 1, r]
 
-                if left == right:
-                    maximum_cookies = max(maximum_cookies, left)
+            if left == right:
+                max_cookie = max(max_cookie, left)
+                l -= 1
+                r += 1
+            elif left < right:
+                l -= 1
+            else:
+                r += 1
 
-    return maximum_cookies
+    return max_cookie
