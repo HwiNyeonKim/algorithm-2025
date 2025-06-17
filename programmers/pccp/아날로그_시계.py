a9c6208129cd85970ec86a053ace1f26fab5ef61
HwiNyeon Kim <z2b23173us@gmail.com>
2025-06-18 03:33:51 +0900

    [Programmers] 아날로그 시계 - Refactor

--- programmers/pccp/아날로그_시계.py
+++ programmers/pccp/아날로그_시계.py
@@ -9,7 +9,7 @@ def solution(h1, m1, s1, h2, m2, s2):
     count = 0
 
     while current < end:
-        # 초침, 분침, 시침이 모두 겹치는 경우에 대한 특수처리
+        # 자정 및 정오에 대한 특수처리
         if current == 0 or current == 12 * 3600:
             count += 1
             current += 1
@@ -19,34 +19,39 @@ def solution(h1, m1, s1, h2, m2, s2):
         minutes = (current % 3600) // 60
         seconds = current % 60
 
+        # 초침 sweep 범위 계산
         second_hand_from = seconds * 6
         second_hand_to = (seconds + 1) * 6
 
+        # 분침 sweep 범위 계산
         minutes_hand_from = minutes * 6 + seconds * 0.1
         minutes_hand_to = minutes * 6 + (seconds + 1) * 0.1
 
+        # 초침과 분침이 겹치는 경우 카운트
+        # 단, 초침의 이동이 종료될 때 겹치는 경우는 카운트하지 않고,
+        # 초침의 이동이 시작될 때 겹치는 경우만 카운트한다.
         if (
             second_hand_from <= minutes_hand_from
-            and minutes_hand_to <= second_hand_to
+            and minutes_hand_to < second_hand_to
         ):
-            count += 1 if minutes_hand_to != 360 else 0
+            count += 1
 
+        # 시침 sweep 범위 계산
         hours_hand_from = hours * 30 + minutes * 0.5 + seconds * (1 / 120)
         hours_hand_to = hours * 30 + minutes * 0.5 + (seconds + 1) * (1 / 120)
 
+        # 초침과 시침이 겹치는 경우 카운트
         if (
             second_hand_from <= hours_hand_from
-            and hours_hand_to <= second_hand_to
+            and hours_hand_to < second_hand_to
         ):
-            # 12시가 되는 순간이 end에 포함된 경우에만 360도에 도달하는 순간을 카운트
-            if hours_hand_to == 360 and end == 12 * 3600:
-                count += 1
-            else:
-                count += 1 if hours_hand_to != 360 else 0
+            count += 1
 
         current += 1
 
-    if h2 != 12 and m2 == 0 and s2 == 0:
+    # 초침의 이동이 종료되는 시점에 겹치는 경우는 카운트하지 않았으므로,
+    # 마지막에 초침과 분침이 12를 가리키며 겹치는 경우를 특수처리로 카운트한다.
+    if m2 == 0 and s2 == 0:
         count += 1
 
     return count
